ec26e86dd78e1b8d92d7ad3e8edb08493f3ebdb6
spawner999 <lore.ferrario@gmail.com>
2016-04-18 11:21:02 -0700

    add test for list immutability

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -17,6 +17,26 @@ describe('immutability', () => {
     });
   });
 
+  describe('A list', () => {
 
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('is immutable', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
+    });    
+  });
 
 });
