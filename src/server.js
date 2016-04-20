e898846be3b1ffe3b78d8e0f6f862b1233919ce4
spawner999 <lore.ferrario@gmail.com>
2016-04-20 14:20:13 -0700

    add some entries, now dispatched to the store after the server runs

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ import Server from 'socket.io';
 
 export default function startServer(store) {
   const io = new Server().attach(8090);
+  console.log("server is running");
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
+  });
+
 }
-
-store.subscribe(
-  () => io.emit('state', store.getState().toJS())
-);
-
-io.on('connection', (socket) => {
-  socket.emit('state', store.getState().toJS());
-});
